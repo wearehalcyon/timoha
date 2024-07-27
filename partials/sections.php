efcc599a0b65c6b33b590a7b97cc3f27bd51bbe7
unknown <thewitness45@gmail.com>
2024-07-27 22:02:28 +0300

    init

--- partials/sections.php
+++ partials/sections.php
@@ -3,18 +3,3 @@
         123
     </div>
 </div>
-<div class="section">
-    <div class="app-container screen-2">
-        123
-    </div>
-</div>
-<div class="section">
-    <div class="app-container screen-3">
-        123
-    </div>
-</div>
-<div class="section">
-    <div class="app-container screen-4">
-        123
-    </div>
-</div>
