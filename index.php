26120b7022816bb3d981c14c45ef57f5c416fe3e
unknown <thewitness45@gmail.com>
2024-07-27 21:00:23 +0300

    init

--- index.php
+++ index.php
@@ -8,6 +8,13 @@
     <link rel="stylesheet" href="./assets/css/app.css" />
 </head>
 <body>
-    
+    <div id="app">
+        <div id="fullpage">
+            <div class="section" id="section1">Section 1</div>
+            <div class="section" id="section2">Section 2</div>
+            <div class="section" id="section3">Section 3</div>
+            <div class="section" id="section4">Section 4</div>
+        </div>
+    </div>
 </body>
 </html>
