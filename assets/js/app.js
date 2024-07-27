4119d11a7c144b1983fa5a5a5957cb830cfece90
unknown <thewitness45@gmail.com>
2024-07-27 21:22:37 +0300

    init

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,15 @@
 'use strict';
 
 jQuery(document).ready(function($){
-    
+    const preloader = $('.preloader');
+    setTimeout(function(){
+        preloader.fadeOut(500);
+    }, 1000);
+
+    // Fullpage
+    new fullpage('#fullpage', {
+        autoScrolling: true,
+        showActiveTooltip: true,
+        scrollingSpeed: 1000
+    });
 });
